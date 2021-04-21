440893c36f77da9e835261354cadb4b1fc0230f0
enverzeynaliev <Robotbender1>
2021-04-21 22:26:31 +0300

    added lab 5 on a new branch

---
+++ lab5.rb
@@ -0,0 +1,75 @@
+require 'socket'
+
+server = TCPServer.new(8000)
+
+class ATM
+  def initialize
+    if File.exists?('balance.txt')
+      balanceValue = File.open('balance.txt', 'r')
+      @balance = balanceValue.read.chomp.to_f
+    else
+      balanceValue = File.open('balance.txt', 'w')
+      balanceValue.puts('100.00')
+      balanceValue.close
+      balanceValue = File.open('balance.txt', 'r')
+      @balance = balanceValue.read.chomp.to_f
+    end
+  end
+
+  def withdraw(withdrawValue)
+    if withdrawValue < 0
+      abort 'Ошибка! Введено отрицательное число.'
+    end
+    if @balance < withdrawValue
+      abort 'Ошибка! Нехватает средств.'
+    end
+    @balance -= withdrawValue
+    balanceValue = File.open('balance.txt', 'w')
+    balanceValue.puts(@balance)
+    balanceValue.close
+    "New balance: #{@balance}"
+  end
+
+  def deposit(depositeValue)
+    if depositeValue < 0
+      abort 'Ошибка! Введено отрицательное число.'
+      exit
+    end
+    @balance += depositeValue
+    balanceValue = File.open('balance.txt', 'w')
+    balanceValue.puts(@balance)
+    balanceValue.close
+    "New balance: #{@balance}"
+  end
+
+  def balance
+    "Balance: #{@balance}"
+  end
+
+end
+
+while connection == server.accept
+
+  ATM = ATM.new
+
+  request = connection.gets
+  m, full_path = request.split(' ')
+  path = full_path.split('/')[1]
+  method = path.split('?')[0]
+  value = path.split('?')[1].split('=')[1].to_i
+
+  connection.print "HTTP/1.1 200\r\n"
+  connection.print "Content-Type: text/html\r\n"
+  connection.print "\r\n"
+
+  connection.print case method
+                   when 'deposit'
+                     ATM.deposit(value)
+                   when 'withdraw'
+                     ATM.withdraw(value)
+                   when 'balance'
+                     ATM.balance
+                   else
+                     'error'
+                   end
+end
